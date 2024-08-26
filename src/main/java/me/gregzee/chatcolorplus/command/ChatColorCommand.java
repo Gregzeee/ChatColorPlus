0f2f1f2ad0bd2aa6b61e45862a1430d86be7d5c6
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-08-26 16:39:48 +0300

    Create reload subcommand

--- src/main/java/me/gregzee/chatcolorplus/command/ChatColorCommand.java
+++ src/main/java/me/gregzee/chatcolorplus/command/ChatColorCommand.java
@@ -1,14 +1,32 @@
 package me.gregzee.chatcolorplus.command;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.StringArgument;
+import me.gregzee.chatcolorplus.ChatColorPlus;
+import me.gregzee.chatcolorplus.manager.ConfigManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
 public class ChatColorCommand {
+
+	private static final ConfigManager configManager = ChatColorPlus.getConfigManager();
 
 	public static void register() {
 		new CommandAPICommand("chatcolorplus")
 				.withAliases("ccp", "chatcolor")
-				.executes(commandExecutor -> {
-
+				.withArguments(new StringArgument("subcommand"))
+				.executes((sender, args) -> {
+					if (args.get("subcommand") == "reload") {
+						if (sender instanceof Player player) {
+							configManager.reload();
+							player.sendMessage(Component.text("Configuration reloaded").color(NamedTextColor.GREEN));
+						} else if (sender instanceof ConsoleCommandSender console) {
+							configManager.reload();
+							console.sendMessage("Configuration reloaded");
+						}
+					}
 				}).register();
 
 	}
